489fda9289600a144d07f015c29f32344dd5c701
linhsuanyu <wayne315315@gmail.com>
2025-04-01 16:21:13 +0800

    Add helper to decouple adaptor from AIPlayer; generate training data on the fly

--- data.py
+++ data.py
@@ -1,9 +1,29 @@
 import random
+import itertools
+from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor
+
+import numpy as np
 
 from player import RandomPlayer
-from game import Game, CARDS
-from tqdm import tqdm
+from game import Game, CARDS, Action
 
+
+def get_reward(scores, exp=2):
+    x = list(zip(scores["total"], scores["coin"])) # eg. [(42, 4), (61,2), (42, 5)]
+    n = len(x)
+    y = [0] * n
+    for i in range(n):
+        for j in range(n):
+            if x[i] > x[j]:
+                y[i] += 1
+            elif x[i] < x[j]:
+                y[i] -= 1
+    y = np.asarray(y)
+    y = np.sign(y) * np.power(np.abs(y), exp)
+    y = y / np.std(y)
+    reward = y[0]
+    return reward
 
 def extract(history):
     n = len(history)
@@ -12,61 +32,81 @@ def extract(history):
     # (api, args, res, is_valid)
     for i in range(n):
         _, [scores, *_], _, _ = history[i].pop() # (api, args, res, is_valid) : (str, list, list, bool)
-        rewards = list(zip(scores["total"], scores["coin"]))
+        reward = get_reward(scores)
         for rec in history[i]:
             api, args, res, is_valid = rec
             if is_valid == True:
-                idx = len(rec_valid[i])
-                rec.append(idx)
-                rec.append(rewards)
-                rec_valid[i].append(rec) # rec: (api, args, res, is_valid, idx, rewards)
+                rec = (api, args, res, reward)
+                rec_valid[i].append(rec) # rec: (api, args, res, reward)
             else:
-                rec_invalid[i].append(rec) # rec: (api, args, res, is_valid)
-    recs = []
+                rec = (api, args, res)
+                rec_invalid[i].append(rec) # rec: (api, args, res)
+    # collect non-hand invalid history
     for i in range(n):
-        for rec in rec_valid[i]:
-            recs.append(rec)
-        for rec in rec_invalid[i]:
-            recs.append(rec)
-    return recs
+        for api, args, res, reward in rec_valid[i]:
+            if api == "move":
+                state, record, hand = args
+                others = set(CARDS) - set(hand) # other cards not in hand
+                for pick, action in itertools.product(others, Action):
+                    rec = (api, args, [pick, action]) # illegal move
+                    rec_invalid[i].append(rec) # rec: (api, args, res)
+    episodes = [(rec_valid[i], rec_invalid[i]) for i in range(n)]
+    return episodes
 
-def data_gen(num_play, num_game):
-    pbar = tqdm(total=num_game * num_play * 5)
-    data = []
-    for n in range(3,8):
-        for num in range(num_game):
-            game = Game(n, random_face=False)
-            players = [RandomPlayer() for _ in range(n)]
-            for i, player in enumerate(players):
-                game.register(i, player)
-            for epoch in range(num_play):
-                history = game.run(verbose=50)
-                data.extend(extract(history))
-                pbar.update(1)
+def epi_gen(args):
+    n, num_play = args
+    game = Game(n, random_face=False)
+    players = [RandomPlayer() for _ in range(n)]
+    episodes = []
+    for i, player in enumerate(players):
+        game.register(i, player)
+    for epoch in range(num_play):
+        history = game.run(verbose=50)
+        episodes.extend(extract(history))
+    random.shuffle(episodes)
+    return episodes
+
+def data_gen(num_play, num_game, shard=10, size=100):
+    data = [[] for _ in range(shard)]
+    args = [(n, num_play) for n in range(3,8) for _ in range(num_game)]
+    random.shuffle(args)
+    with ProcessPoolExecutor() as executor:
+        for episodes in executor.map(epi_gen, args):
+            for episode in episodes:
+                i = random.randint(0, shard-1)
+                data[i].append(episode)
+                # shard reach size
+                if len(data[i]) >= size:
+                    random.shuffle(data[i])
+                    while data[i]:
+                        yield data[i].pop()
     random.shuffle(data)
-    pbar.close()
-    return data
-
-"""   
-def meow(x, exp=2):
-    x = np.asarray(x)
-    x = np.sign(x) * np.power(np.abs(x), exp)
-    return x / np.std(x)
-"""
+    for datum in data:
+        random.shuffle(datum)
+        while datum:
+            yield datum.pop()
 
 # data generation
-# Each game has around 18 moves for each player
 # Same game plays for 100 times
 # Same player number plays for 100 times
 # Player number : 3 - 7
-# total data : 18 * (3+4+5+6+7) * 100 * 100 = 4500000 at least
+# total data : (3+4+5+6+7) * 100 * 100 = 250000 at least
 
 if __name__ == "__main__":
+    from tqdm import tqdm
+    from helper import Adaptor
+    adaptor = Adaptor()
     num_play = 2 # The number of rehearsals for each game
     num_game = 2 # The number of games for each number of the total players
     data = data_gen(num_play, num_game)
-    for rec in data:
-        api, args, res, is_valid, *extras = rec
-        if is_valid:
-            idx, rewards = extras
-            print(api,res,idx,rewards)
+    for episode in tqdm(data, total=num_play * num_game * 25):
+        rec_valid, rec_invalid = episode
+        print("")
+        print(len(rec_valid))
+        for rec in rec_valid:
+            api, args, res, reward = rec
+            print(api, reward)
+        print("")
+        print(len(rec_invalid))
+        for rec in rec_invalid:
+            api, args, res = rec
--- rl.py
+++ rl.py
@@ -1,97 +1,31 @@
 import random
 
-import numpy as np
-
+from helper import Adaptor
 from player import Player
-from const import Action, CARDS, CIVS
 
 
 class AIPlayer(Player):
     def __init__(self, model):
         super().__init__()
-        self.civs = [None] + list(CIVS)
-        self.faces = [None] + ["Day", "Night"]
-        self.cards = [None] + list(CARDS)
-        self.action = [None] + list(Action)
-        self.civ2idx = {civ: i for i, civ in enumerate(self.civs)}
-        self.face2idx = {face: i for i, face in enumerate(self.faces)}
-        self.card2idx = {card: i for i, card in enumerate(self.cards)}
-        self.action2idx = {action: i for i, action in enumerate(self.action)}
+        self.helper = Adaptor()
         self.model = model
 
-    def s2v(self, state, record):
-        # determine the number of players
-        n = len(state)
-        # (turn, card, action, pos, civ, face)
-        v = []
-        # priming
-        for pos, s in enumerate(state):
-            i_civ = self.civ2idx[s["civ"]]
-            i_face = self.face2idx[s["face"]]
-            v.append([0, 0, 0, pos, i_civ, i_face])
-
-        # other players
-        for pos, s in enumerate(state):
-            i_civ = self.civ2idx[s["civ"]]
-            i_face = self.face2idx[s["face"]]
-            if pos:
-                for turn, card in s["built"]:
-                    i_card = self.card2idx[card]
-                    i_action = self.action2idx[Action.BUILD]
-                    v.append([turn, i_card, i_action, pos, i_civ, i_face])
-                for turn in s["wonder"]:
-                    i_card = self.card2idx[None] # 0
-                    i_action = self.action2idx[Action.WONDER]
-                    v.append([turn, i_card, i_action, pos, i_civ, i_face])
-                for turn in s["discard"]:
-                    i_card = self.card2idx[None]
-                    i_action = self.action2idx[Action.DISCARD]
-                    v.append([turn, i_card, i_action, pos, i_civ, i_face])
-            else:
-                i_civ_player = self.civ2idx[s["civ"]]
-                i_face_player = self.face2idx[s["face"]]
-        # player itself
-        for turn, card, action, hand in record:
-            i_card = self.card2idx[card]
-            i_action = self.action2idx[action]
-            v.append([turn, i_card, i_action, 0, i_civ_player, i_face_player])
-        
-        # padding
-        # (turn, card, action, pos, civ, face)
-        m = 19 * n + 6 - len(v) # max total : Prime turn n + 18 turn * n player + 3 extra turn (Babylon) + 3 extra turn (Halikarnassos)
-        for _ in range(m):
-            v.append([19, 0, 0, n, 0, 0])
-
-        v = np.asarray(v)
-        return v
-
-    def h2v(self, hand):
-        h = np.asarray([self.card2idx[card] for card in hand])
-        return h
-
-    def c2v(self, coins):
-        pass
-
     def send_face(self, state):
-        v = self.s2v(state, [])
+        v = self.helper.s2v(state, [])
         face = random.choice(["Day", "Night"]) # [[API_CALL, *args, *res, is_valid]]
         self.record.append(["face", [state], [face], True])
         return face
 
     def send_move(self, state, record, hand, asked):
         if not asked:
-            v = self.s2v(state, record) # shape (18 * n + 6, 6)
-            h = self.h2v(hand)
-            v = np.expand_dims(v, axis=0)
-            h = np.expand_dims(h, axis=0)
+            v = self.helper.s2v(state, record) # shape (18 * n + 6, 6)
+            h = self.helper.h2v(hand)
             self.buffer = self.model.predict_move(v, h)[0].tolist()
         else:
             self.record[-1][-1] = False # last move is invalid
 
         output = self.buffer.pop()
-        i_card, i_action = divmod(output, len(Action))
-        pick = self.cards[i_card + 1]
-        action = self.action[i_action + 1]
+        pick, action = self.helper.pair[output]
         self.record.append(["move", [state, record, hand], [pick, action], True])
         return (pick, action)
 
@@ -103,11 +37,12 @@ class AIPlayer(Player):
     def recv_score(self, scores):
         self.record.append(["score", [scores], [], True])
 
+
 if __name__ == "__main__":
     import tensorflow as tf
     from player import RandomPlayer
     from model import ActorCritic
-    from game import Game
+    from game import Game, CARDS
     model = ActorCritic(len(CARDS), 100)
     n = 3
     game = Game(n)
