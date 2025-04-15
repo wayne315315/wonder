3f639d010eb38aa3d197b1ee8b596b5c42852f79
linhsuanyu <wayne315315@gmail.com>
2025-04-15 15:32:31 +0800

    Train on batch

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import random
-import itertools
+from collections import defaultdict
 
 from tqdm import tqdm
 import tensorflow as tf
@@ -36,7 +36,7 @@ def translate(episode, gamma=0.9, penalty=-1.0):
     """
     # Task 4 : add penalty to rec_invalid & sample n examples from rec_invalid
     if rec_invalid:
-        rec_invalid_ = [[rec[0], rec[1], rec[2], penalty] for rec in rec_invalid]
+        rec_invalid_ = [[rec[0], rec[1], rec[2], penalty / n] for rec in rec_invalid]
         random.shuffle(rec_invalid_)
         m = len(rec_invalid)
         n = len(rec_valid)
@@ -82,17 +82,22 @@ def compute_loss(logits, values, actions, rewards):
     # loss = loss_actor + loss_critic
     # tf.gather(x, y, batch_dims=1)
     # TensorShape([None, 231]) TensorShape([None, 1]) TensorShape([None]) TensorShape([None])
-    huber_loss = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)
+    huber_loss = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.SUM)
     values = values[:,0] # v(s) TensorShape([None])
     loss_critic = huber_loss(rewards, values) # TensorShape([])
     advantages = rewards - values # (g(a,s) - v(s))  TensorShape([None])
     logsoftmax = tf.nn.log_softmax(logits, axis=1) # log(p(ai|s), ...) TensorShape([None, 231])
     logsoftmax = tf.gather(logsoftmax, actions, batch_dims=1) # log(p(a|s)) TensorShape([None])
-    loss_actor = -tf.reduce_mean(logsoftmax * advantages)
-    loss = loss_critic + loss_actor
-    return loss, loss_actor, loss_critic
+    loss_actor = -tf.reduce_sum(logsoftmax * advantages)
+    #loss = loss_critic + loss_actor
+    loss = -tf.reduce_sum(logsoftmax * rewards)
+    # metrics
+    prob = tf.nn.softmax(logits, axis=1) # p(a|s) TensorShape([None, 231])
+    prob = tf.gather(prob, actions, batch_dims=1) # p(a|s) TensorShape([None])
+    expected_return = tf.reduce_sum(prob * rewards) # E[R|a,s] TensorShape([])
+    return loss, loss_actor, loss_critic, tf.reduce_sum(prob), expected_return
 
-def train(model_path, epoch, num_play, num_game, gamma=0.99, penalty=-10.0, run_episode=True):
+def train(model_path, epoch, num_play, num_game, gamma=0.99, penalty=-1.0, run_episode=True):
     # model
     model = tf.keras.models.load_model(model_path) if model_path.exists() else create_model()
     # optimizer
@@ -108,40 +113,65 @@ def train(model_path, epoch, num_play, num_game, gamma=0.99, penalty=-10.0, run_
     def train_step(vs, hs, ys, rs):
         with tf.GradientTape() as tape:
             logits, values = model(vs, hs)
-            loss, loss_actor, loss_critic = compute_loss(logits, values, ys, rs)
+            loss, loss_actor, loss_critic, prob, expected_return = compute_loss(logits, values, ys, rs)
         grads = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(grads, model.trainable_variables))
-        return loss, loss_actor, loss_critic
+        return loss, loss_actor, loss_critic, prob, expected_return
     # training loop
     for e in range(epoch):
         losses = []
         losses_actor = []
         losses_critic = []
+        probs = []
+        expected_returns = []
         data_iterator = data_gen(num_play, num_game, model=model) if run_episode else data_gen(num_play, num_game)
-        for episode in tqdm(data_iterator, total=25*num_play*num_game):
-            vs, hs, ys, rs = translate(episode, gamma=gamma, penalty=penalty)
-            loss, loss_actor, loss_critic = train_step(vs, hs, ys, rs)
+        vs = defaultdict(list)
+        hs = defaultdict(list)
+        ys = defaultdict(list)
+        rs = defaultdict(list)
+        total = 25 * num_play * num_game
+        for episode in tqdm(data_iterator, total=total):
+            v, h, y, r = translate(episode, gamma=gamma, penalty=penalty)
+            vs[(v.shape, h.shape)].append(v)
+            hs[(v.shape, h.shape)].append(h)
+            ys[(v.shape, h.shape)].append(y)
+            rs[(v.shape, h.shape)].append(r)
+        for key in vs:
+            loss, loss_actor, loss_critic, prob, expected_return  = train_step(
+                tf.concat(vs[key], axis=0), 
+                tf.concat(hs[key], axis=0), 
+                tf.concat(ys[key], axis=0), 
+                tf.concat(rs[key], axis=0)
+            )
             losses.append(loss.numpy())
             losses_actor.append(loss_actor.numpy())
             losses_critic.append(loss_critic.numpy())
+            probs.append(prob.numpy())
+            expected_returns.append(expected_return.numpy())
+            print("prob: %.2E" % probs[-1])
+            print("expected return: %.2E" % expected_returns[-1])            
         # save model
-        loss_avg = sum(losses)/len(losses)
-        loss_actor_avg = sum(losses_actor)/len(losses_actor)
-        loss_critic_avg = sum(losses_critic)/len(losses_critic)
+        loss_avg = sum(losses)/total
+        loss_actor_avg = sum(losses_actor)/total
+        loss_critic_avg = sum(losses_critic)/total
+        prob_avg = sum(probs)/total
+        expected_return_avg = sum(expected_returns)/total
         print("epoch:", e)
         print("loss: %.2E" % loss_avg)
         print("loss actor: %.2E" % loss_actor_avg)
         print("loss critic: %.2E" % loss_critic_avg)
+        print("prob: %.2E" % prob_avg)
+        print("expected return: %.2E" % expected_return_avg)
         model.save(model_path)
         print("model saved")
 
 if __name__ == "__main__":
     epoch = 1000
-    num_play = 8 # The number of rehearsals for each game
-    num_game = 8 # The number of games for each number of the total players
+    num_play = 1 # The number of rehearsals for each game
+    num_game = 4 # The number of games for each number of the total players
     # model
     model_dir = Path("model")
     if not model_dir.exists():
         model_dir.mkdir()
     model_path = Path(model_dir, "ac.keras")
-    train(model_path, epoch, num_play, num_game)
+    train(model_path, epoch, num_play, num_game, run_episode=False)
