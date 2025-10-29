Save the model before apply gradients to ensure the metrics match the model; Fix the discounted reward; Only include the data generated from the actor itself for training; Use AIPlayer2 to generate data; Develop PPO loss for exploit.py; train multiple steps within each epoch with the same data and PPO policy

web.py
@@ -151,7 +151,7 @@ class WebAIExploiter(AIPlayer, WebPlayer):
     model = None
     def __init__(self, uid, url, events):
         if not WebAIExploiter.model:
-            model_path = Path("model", "exploiter.keras")
+            model_path = Path("model", "exploiter_best.keras")
             WebAIExploiter.model = tf.keras.models.load_model(model_path)
         AIPlayer.__init__(self, WebAIExploiter.model)
         WebPlayer.__init__(self, uid, url, events)