028c53a59c58a3e5594b8e23eade9e20f7661315
linhsuanyu <wayne315315@gmail.com>
2025-11-19 21:33:28 +0800

    Finish tf ray cluster flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
-from pathlib import Path
+import ray
+from ftp import RayFTPClient
 
 import tensorflow as tf
 from tensorflow.keras import mixed_precision
@@ -9,11 +10,18 @@ from data import write_data
 from train import train
 
 
-# Faster with CPU rather than GPU
-tf.config.set_visible_devices([], 'GPU')
+# master node generate data
+def master(p_data, p_model, p_other="", num_game=10, policy="mixed_float16"):
+    t1 = time.time()
+    # download model from FTP server
+    with RayFTPClient() as ftp:
+        # download the latest model
+        ftp.download(p_model)
 
-#write_data(p_data, num_game, fn_model, fn_others=[fn_other])
-def gen_data(p_data, p_model, p_other="", num_game=10):
+    # set policy
+    mixed_precision.set_global_policy(policy)
+
+    # load model and concrete function
     model = tf.keras.models.load_model(p_model)
     fn_model = tf.function(lambda state, hand: model([state, hand])[:2]).get_concrete_function(
             tf.TensorSpec(shape=[None, None, 7], dtype=tf.int32),
@@ -28,29 +36,55 @@ def gen_data(p_data, p_model, p_other="", num_game=10):
     else:
         fn_other = None
     # write TFRecord
-    t1 = time.time()
     write_data(p_data, num_game, fn_model, fn_others=[fn_other])
+    # upload data to FTP server
+    with RayFTPClient() as ftp:
+        ftp.upload(p_data)
     t2 = time.time()
-    print(f"Gen data Time taken: {t2 - t1} seconds")
+    print(f"Master took: {t2 - t1} seconds")
 
 
-def main(p_model, p_other="", p_data="data/meow.tfrecord", policy="mixed_float16", round=100):
+@ray.remote(num_gpus=1, resources={"arch_x86": 0.999, "gpu_type_cuda": 1})
+def worker(p_data, p_model, policy="mixed_float16", round=100, batch_size=128):
+    t1 = time.time()
+    import tensorflow as tf
+    from tensorflow.keras import mixed_precision
+    from model import ActorCritic
+    from model_fn import create_ac
+    from train import train
+    # download data and model from FTP server
+    with RayFTPClient() as ftp:
+        ftp.download(p_data)
+        ftp.download(p_model)
+
+    # set policy
+    mixed_precision.set_global_policy(policy)
+    # train
+    train(p_data, p_model, epoch=10, learning_rate=1e-4, batch_size=batch_size)
+
+    # upload model to FTP server
+    with RayFTPClient() as ftp:
+        ftp.upload(p_model)
+
+    t2 = time.time()
+    print(f"Master took: {t2 - t1} seconds")
+
+
+def main(p_model, p_other="", p_data="data/woof.tfrecord", policy="mixed_float16", round=100):
     mixed_precision.set_global_policy(policy)
     # path
     for r in range(round):
-        gen_data(p_data, p_model, p_other=p_other)
-        t1 = time.time()
-        train(p_data, p_model, epoch=10, learning_rate=1e-4, batch_size=4096)
-        t2 = time.time()
-        print(f"Train Time taken: {t2 - t1} seconds")
+        master(p_data, p_model, p_other=p_other)
+        future = worker.remote(p_data, p_model, policy="mixed_float16", round=100, batch_size=128)
+        ray.get(future)
         print(f"Round {r} completed", flush=True)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="model_float16/meow.keras")
+    parser.add_argument("--model", type=str, default="model_float16/woof.keras")
     parser.add_argument("--other", type=str, default="")
-    parser.add_argument("--data", type=str, default="data/meow.tfrecord")
+    parser.add_argument("--data", type=str, default="data/woof.tfrecord")
     parser.add_argument("--policy", type=str, default="mixed_float16")
     parser.add_argument("--round", type=int, default=100)
     args = parser.parse_args()
